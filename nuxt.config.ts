124c23f5f87fb6c7be016862f9455786f80bbb64
chrispaganon <info@chrispaganon.com>
2023-07-29 11:18:29 -0400

    Add css reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
+    "assets/base.css",
     "assets/theme/themes/material/material-dark/standard/indigo/theme.scss"
   ],
   build: {
