d0cc07e84b80f8f4bda7226050ce5c240bd9f749
chrispaganon <info@chrispaganon.com>
2023-07-29 15:15:32 -0400

    Test adding loading state to avoid layout shift

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    <AppHeader />
-    <slot />
-    <AppFooter />
+    <div v-if="loading">
+      <h1>loading</h1>
+    </div>
+    <div v-else>
+      <AppHeader />
+      <slot />
+      <AppFooter />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+console.log('Layout setup');
+const loading = ref(true);
+
+onMounted(() => {
+  console.log('Layout mounted');
+  loading.value = false;
+});
+</script>
