fc5db996828a5a261b6fb4cbde3f3514346df520
chrispaganon <info@chrispaganon.com>
2023-07-29 16:25:28 -0400

    Add loader with content pre-rendered under

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <AppHeader />
-    <slot />
-    <AppFooter />
+    <div v-if="loading" class="fixed top-0 left-0 w-screen h-screen flex justify-content-center align-items-center surface-overlay z-5">
+      <h1>loading</h1>
+    </div>
+    <div>
+      <AppHeader />
+      <slot />
+      <AppFooter />
+    </div>
   </div>
 </template>
 
@@ -10,7 +15,7 @@
 console.log('Layout setup');
 const loading = ref(true);
 
-onMounted(() => {
+onBeforeMount(() => {
   console.log('Layout mounted');
   loading.value = false;
 });
