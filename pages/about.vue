0b5a41aed7fd521a663284eebaa93f6f005c40aa
chrispaganon <info@chrispaganon.com>
2023-07-29 11:35:45 -0400

    Test pages routing & pre-rendering

---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>ABOUT</h2>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
   <div>
     <h1>HEADING</h1>
     <Button label="TEST" />
+    <NuxtLink to="/about">About</NuxtLink>
   </div>
 </template>
