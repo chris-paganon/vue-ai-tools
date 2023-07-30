b530ba6595262b36d049bad010e97d93eb8b2739
chrispaganon <info@chrispaganon.com>
2023-07-30 16:27:42 -0400

    Improve intro text

--- components/sections/Introduction.vue
+++ components/sections/Introduction.vue
@@ -1,5 +1,5 @@
 <template>
   <section class="max-w-1024 mx-auto px-4 pt-8">
-    <p class="mb-6">Your go-to resource for chatbot assistance tailored specifically for VueJS. Our GPT-Powered chatbot is finely tuned to provide <strong>more helpful answers</strong> and to <strong>minimize errors and hallucinations</strong>.</p>
+    <p class="mb-6">Your go-to resource for AI tools tailored specifically for VueJS. Our GPT-Powered assistant is finely tuned to provide <strong>more helpful answers</strong> and to <strong>minimize errors and hallucinations</strong>.</p>
   </section>
 </template>
