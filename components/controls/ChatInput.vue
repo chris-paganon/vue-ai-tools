129b1409283d1fbbf04ca1c245ae90ffda973dee
chrispaganon <info@chrispaganon.com>
2023-07-30 11:58:16 -0400

    Move chat input into seperate component

---
+++ components/controls/ChatInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-content-between gap-3 my-3">
+    <InputText v-model="inputQuestion" type="text" placeholder="What is the composition API?" class="w-10" />
+    <Button @click="askQuestion" label="Ask" size="large" class="flex-grow-1" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const inputQuestion = ref('');
+
+const askQuestion = () => {
+  useAskQuestion(inputQuestion.value);
+}
+</script>
