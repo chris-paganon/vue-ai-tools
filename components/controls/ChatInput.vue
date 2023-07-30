39fbcf8c82ec1f6e21549d24b6b535fe46615aa9
chrispaganon <info@chrispaganon.com>
2023-07-30 12:32:12 -0400

    Ask question on press enter as well

--- components/controls/ChatInput.vue
+++ components/controls/ChatInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-content-between gap-3 my-3">
-    <InputText v-model="inputQuestion" type="text" placeholder="What is the composition API?" class="w-10" />
-    <Button @click="askQuestion" label="Ask" size="large" class="flex-grow-1" />
+    <InputText v-model="inputQuestion" @keyup.enter="askQuestion()" type="text" placeholder="What is the composition API?" class="w-10" />
+    <Button @click="askQuestion()" label="Ask" size="large" class="flex-grow-1" />
   </div>
 </template>
 
