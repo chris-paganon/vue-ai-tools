4da9e19e32ac09ebab3a3ee091abd4645cb61556
chrispaganon <info@chrispaganon.com>
2023-07-30 21:57:14 -0400

    Another worker test

--- server/api/completion.post.ts
+++ server/api/completion.post.ts
@@ -5,19 +5,22 @@ export default defineEventHandler(async (event) => {
   const messages = body.messages;
 
   const { cloudflare } = event.context;
+  console.log(cloudflare);
+  
   const configuration = new Configuration({
     organization: cloudflare.env.OPENAI_ORGANIZATION,
     apiKey: cloudflare.env.OPENAI_API_KEY,
   });
+  const answer = JSON.stringify(configuration);
   
-  const openai = new OpenAIApi(configuration);
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: messages,
-  });
+  // const openai = new OpenAIApi(configuration);
+  // const response = await openai.createChatCompletion({
+  //   model: "gpt-3.5-turbo",
+  //   messages: messages,
+  // });
 
-  if (!response.data.choices[0].message?.content) return;
-  const answer = JSON.stringify(response.data.choices[0].message.content);
+  // if (!response.data.choices[0].message?.content) return;
+  // const answer = JSON.stringify(response.data.choices[0].message.content);
 
   return answer;
 });
