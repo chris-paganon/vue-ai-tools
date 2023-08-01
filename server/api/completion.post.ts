c2985d4644c727044963dc6b5d1b06170eeb0079
chrispaganon <info@chrispaganon.com>
2023-07-31 21:44:49 -0400

    Enable local dev for cloudflare worker

--- server/api/completion.post.ts
+++ server/api/completion.post.ts
@@ -3,16 +3,28 @@ export default defineEventHandler(async (event) => {
   const messages = body.messages;
   const data = {
     model: "gpt-3.5-turbo",
+    temperature: 1,
     messages: messages,
   };
 
-  const { cloudflare } = event.context;
+  let openaiKey = '';
+  let openaiOrganization = '';
+  if (event.context.cloudflare) {
+    const { cloudflare } = event.context;
+    openaiKey = cloudflare.env.OPENAI_API_KEY;
+    openaiOrganization = cloudflare.env.OPENAI_ORGANIZATION;
+  } else {
+    const runtimeConfig = useRuntimeConfig();
+    openaiKey = runtimeConfig.public.openaiApiKey;
+    openaiOrganization = runtimeConfig.public.openaiOrganization;
+  }
+  
   const responseJson = await fetch('https://api.openai.com/v1/chat/completions', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${cloudflare.env.OPENAI_API_KEY}`,
-      'Openai-Organization': cloudflare.env.OPENAI_ORGANIZATION
+      'Authorization': `Bearer ${openaiKey}`,
+      'Openai-Organization': openaiOrganization,
     },
     body: JSON.stringify(data),
   });
