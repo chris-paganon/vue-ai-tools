77c592cdce29d6f8fb72b73ad6d73e66775fcd23
chrispaganon <info@chrispaganon.com>
2023-08-07 22:25:23 -0400

    Add some TODOs and move a bit of code

--- composables/stores/useChatFunctionsStore.ts
+++ composables/stores/useChatFunctionsStore.ts
@@ -6,7 +6,7 @@ export const useChatFunctionsStore = defineStore('chatFunctions', () => {
     name: 'answerQuestionWithDocumentation',
     description: 'Returns the relevant VueJS documentation page from its title.',
     parameters: {
-      "type": 'object', // Should be array of strings to select several pages
+      "type": 'object', // Should be array of strings to select several pages and validate the title dynamically. The array of strings may be generated with the python script.
       "properties": {
         "title": {
           "type": 'string',
@@ -23,9 +23,9 @@ export const useChatFunctionsStore = defineStore('chatFunctions', () => {
     const functionArguments = JSON.parse(response[0].message.function_call.arguments);
     if (! functionArguments.title ) return;
 
+    // TODO: Handle the function dynamically instead of switch statement?
     switch (response[0].message.function_call.name) {
       case 'answerQuestionWithDocumentation':
-        // TODO: Check the right index (option vs composition) and validate arguments dynamically
         answerQuestionWithDocumentation(functionArguments);
         break;
       default:
@@ -36,6 +36,9 @@ export const useChatFunctionsStore = defineStore('chatFunctions', () => {
 
   function answerQuestionWithDocumentation(functionArguments: any) {
     const { addAssistantMessage } = useChatStore();
+    const { replaceSystemMessage } = useChatStore();
+
+    // TODO: Check the right index (option vs composition) and validate arguments dynamically
     const compositionPage = compositionIndex.find((compositionPage) => compositionPage.title === functionArguments.title);
     if ( !compositionPage ) {
       console.log('Function argument invalid');
@@ -43,10 +46,9 @@ export const useChatFunctionsStore = defineStore('chatFunctions', () => {
       return;
     };
 
-    const { replaceSystemMessage } = useChatStore();
-
+    // TODO: Not strong enough wording. GPT basically doesn't use it.
     replaceSystemMessage(`Here is the VueJS documentation page called ${functionArguments.title} that may be relevant to the user question: {{VAI_DOC_PAGE}}`);
-    // TODO: Have the path is JSON file instead of the URL
+    // TODO: Have the path in JSON file instead of the URL
     const path = compositionPage.url.replace('https://vuejs.org/', '').replace('.html', '.composition.md');
     useAskDocCompletion(path);
     addAssistantMessage(`<p>Here are relevant URLs from the documentation: <a target="_blank" href="${compositionPage.url}">${compositionPage.url}</a>.</p> <p>Feeding the pages to GPT to give you a more precise answer...</p>`); 
