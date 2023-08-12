ab991b4680a73be2582a8eca146c1b3c08126862
chrispaganon <info@chrispaganon.com>
2023-08-12 13:24:49 -0400

    Better match loader overlay & hero banner overlay

--- components/AppLoader.vue
+++ components/AppLoader.vue
@@ -28,7 +28,14 @@ onNuxtReady(() => {
   left: 0;
   width: 100%;
   height:100%;
-  background-image: linear-gradient(rgba(0,0,0,0.95), rgba(0,0,0,0.9) 15%, rgba(0,0,0,0.60) 60%, rgba(0,0,0,0.2));
+  background-image: linear-gradient(
+    rgba(0,0,0,0.78), 
+    rgba(0,0,0,0.76) 35%,
+    rgba(0,0,0,0.69) 50%, 
+    rgba(0,0,0,0.3) 70%,
+    rgba(0,0,0,0.4) 95%,
+    var(--surface-ground) 100%
+  );
 }
 .loader-overlay::after,
 .loader-overlay::before {
@@ -40,7 +47,7 @@ onNuxtReady(() => {
   height:100%;
 }
 .loader-overlay::after {
-  background: radial-gradient(rgb(0,0,0,0.9), rgba(0,0,0,1) 83%, rgba(0,0,0,1) 100%);
+  background: radial-gradient(rgb(0,0,0,0.1), rgba(0,0,0,0.2) 40%, rgba(0,0,0,1) 100%);
 }
 .loader-overlay::before {
   background-color: #000;
@@ -48,20 +55,22 @@ onNuxtReady(() => {
 
 .v-leave-active {
   transition: 
-    opacity 1.4s cubic-bezier(0.7, 0, 1, 0.48) 0.5s, 
-    max-height 0.8s cubic-bezier(0.52, 0.26, 0, 1) 0.5s, 
-    scale 0.8s cubic-bezier(0.52, 0.26, 0, 1) 0.4s;
+    opacity 1s cubic-bezier(0.7, 0, 1, 0.48) 0.8s, 
+    max-height 0.8s cubic-bezier(0.69, 0.56, 0.42, 0.97) 0.4s,
+    scale 0.8s cubic-bezier(0.69, 0.56, 0.42, 0.97) 0.4s;
 }
+/* Disappearing completely black initial loader overlay */
 .v-leave-active .loader-overlay::before {
-  transition: opacity 0.3s cubic-bezier(0.89, 0, 0.68, 0.74);
+  transition: opacity 0.6s cubic-bezier(0.89, 0, 0.68, 0.74);
 }
+/* Disappearing fish eye overlay */
 .v-leave-active .loader-overlay::after {
   transition: opacity 0.4s cubic-bezier(0.89, 0, 0.68, 0.74) 0.2s;
 }
 .v-leave-from {
   opacity: 1;
   max-height: 100%;
-  scale: 1.2;
+  scale: 1.4;
 }
 .v-leave-from .loader-overlay::after,
 .v-leave-from .loader-overlay::before {
