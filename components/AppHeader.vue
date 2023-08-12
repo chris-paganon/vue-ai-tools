ba504e16cbd8bd54e4c18cc5a8e5e8a697e6fd51
chrispaganon <info@chrispaganon.com>
2023-08-11 23:12:22 -0400

    Add new hero banner

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,15 +1,9 @@
 <template>
-  <header>
-    <Menubar class="border-noround">
-      <template #start>
-        <NuxtLink to="/">
-          <img src="/img/logo-nobg.png" alt="Logo" class="logo" width="60" />
-        </NuxtLink>
-      </template>
-      <template #end>
-        <Button @click="setIsChatOpened(true)">Open Chat</Button>
-      </template>
-    </Menubar>
+  <header class="flex justify-content-between align-items-center px-4 py-2">
+    <NuxtLink to="/">
+      <img src="/img/logo-nobg.png" alt="Logo" class="logo" width="60" />
+    </NuxtLink>
+    <Button @click="setIsChatOpened(true)">Open Chat</Button>
   </header>
 </template>
 
--- components/sections/HeroBanner.vue
+++ components/sections/HeroBanner.vue
@@ -1,19 +1,19 @@
 <template>
-  <section class="hero-banner h-16rem relative">
+  <section class="hero-banner h-30rem relative">
     <div class="hero-banner-overlay absolute w-full h-full flex flex-column justify-content-center align-items-center">
       <h1 class="text-center">VueTools.ai</h1>
-      <h2 class="text-2xl text-center">GPT-Powered Assistant, Fine-tuned for VueJS Documentation</h2>
+      <h2 class="text-2xl text-center">GPT-Powered Assistant for VueJS Documentation</h2>
     </div>
   </section>
 </template>
 
 <style scoped>
 .hero-banner {
-  background-image: url('/img/vuejs-banner.jpg');
+  background-image: url('/img/Hero-banner_midjourney.png');
   background-size: cover;
-  background-position: center top;
+  background-position: center center;
 }
 .hero-banner-overlay {
-  background-color: rgba(0, 0, 0, 0.6);
+  background: radial-gradient(rgb(0,0,0,0.78), rgba(0,0,0,0.68) 35%, rgba(0,0,0,0.1) 80%, rgba(0,0,0,0.4) 100%);
 }
 </style>
