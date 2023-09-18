8dcaac3d4c35857392f433302bc5e08555bac6a3
chrispaganon <info@chrispaganon.com>
2023-09-18 12:14:16 -0400

    Add basic umami data collection

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,3 +9,15 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      async: true,
+      src: 'http://159.203.58.85:3000/script.js',
+      'data-website-id': '716766be-9975-4463-be2d-2a8b39c8d4cb',
+    }
+  ],
+});
+</script>
