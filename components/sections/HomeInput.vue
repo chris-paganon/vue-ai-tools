b41fce585f0bc312ffd0f705ecd54564de04eee3
chrispaganon <info@chrispaganon.com>
2023-09-27 01:42:30 -0400

    Remove vue 2 warning

--- components/sections/HomeInput.vue
+++ components/sections/HomeInput.vue
@@ -2,6 +2,5 @@
   <section class="max-w-1024 mx-auto px-2 md:px-4 pb-6">
     <h3 class="text-xl">Ask your VueJS question here:</h3>
     <ChatInputControl />
-    <p class="text-sm"><em>Fine-tuned for Vue 3 only. Vue 2 integration is in progress.</em></p>
   </section>
 </template>
