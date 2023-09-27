c71a58445f4035d75d16a33fa7078d82ba7e3acd
chrispaganon <info@chrispaganon.com>
2023-09-27 01:51:38 -0400

    Test deployment

--- components/sections/HomeInput.vue
+++ components/sections/HomeInput.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="max-w-1024 mx-auto px-2 md:px-4 pb-6">
-    <h3 class="text-xl">Ask your VueJS questions here:</h3>
+    <h3 class="text-xl">Ask your VueJS question here:</h3>
     <ChatInputControl />
   </section>
 </template>
