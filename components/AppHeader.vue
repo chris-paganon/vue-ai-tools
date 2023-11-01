46b8d8e0ecc624ea5cadf923f2da08f643d537ba
chrispaganon <info@chrispaganon.com>
2023-11-01 15:20:53 -0400

    Add prototype logged in state

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,13 +3,51 @@
     <NuxtLink to="/">
       <img src="/img/logo-nobg.png" alt="Logo" class="logo" width="60" />
     </NuxtLink>
-    <div class="flex gap-4">
-      <Button @click="setIsLoginModalOpened(true)" severity="secondary" outlined>Login</Button>
-      <Button @click="setIsSignUpModalOpened(true)">Sign Up</Button>
+    <div v-if="!isLoadingAuth">
+      <div v-if="!isSignedIn" class="flex gap-4">
+        <Button @click="setIsLoginModalOpened(true)" severity="secondary" outlined>Login</Button>
+        <Button @click="setIsSignUpModalOpened(true)">Sign Up</Button>
+      </div>
+      <div v-else class="flex gap-4">
+        <Button @click="logout" severity="secondary" outlined>Log out</Button>
+      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import PocketBase from 'pocketbase';
+const pbUrl = useRuntimeConfig().public.pocketbaseUrl;
+const pb = new PocketBase(pbUrl);
+
 const { setIsSignUpModalOpened, setIsLoginModalOpened } = useUIStore();
+
+function logout() {
+  if (pb.authStore.isValid) {
+    pb.authStore.clear();
+    window.location.reload();
+  }
+}
+
+const isSignedIn = ref(false);
+const isLoadingAuth = ref(true);
+
+onMounted(async () => {  
+  if (!pb.authStore.isValid) {
+    isSignedIn.value = false;
+    isLoadingAuth.value = false;
+    return;
+  }
+  isSignedIn.value = true
+
+  // Refresh auth just in case
+  await pb.collection('users').authRefresh();
+  if (pb.authStore.isValid) {
+    isSignedIn.value = true;
+    isLoadingAuth.value = false;
+    return;
+  };
+  isSignedIn.value = false;
+  isLoadingAuth.value = false;
+});
 </script>
