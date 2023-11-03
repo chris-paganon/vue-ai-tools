81107f1201e9cb5db69bc6b50c17d1387789d781
chrispaganon <info@chrispaganon.com>
2023-11-03 17:47:57 -0400

    Improve dashboard design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,6 +52,7 @@ export default defineNuxtConfig({
         'Message',
         'Checkbox',
         'Toast',
+        'Card',
       ]
     },
   },
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,12 +3,16 @@
     <div class="py-4 px-6 border-right-1 border-50">
       <h2>Menu</h2>
     </div>
-    <div class="flex-grow-1 flex flex-column p-4">
-      <h1>Dashboard</h1>
-      <div class="flex-grow-1 flex flex-column justify-content-end border-round-lg surface-overlay p-4">
-        <ChatConversation />
-        <ChatInputControl />
-      </div>
+    <div class="flex-grow-1 flex flex-column ml-4 mr-2 mb-2">
+      <h1 class="ml-4">Dashboard</h1>
+      <Card class="flex-grow-1 flex flex-column justify-content-end">
+        <template #content>
+          <ChatConversation />
+        </template>
+        <template #footer>
+          <ChatInputControl />
+        </template>
+      </Card>
     </div>
   </main>
 </template>
