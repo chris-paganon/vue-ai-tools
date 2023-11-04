657e25d689bdaa2499635100223a00974f158a9a
chrispaganon <info@chrispaganon.com>
2023-11-03 23:46:50 -0400

    Improve verifyAuth & logout

--- composables/stores/useAuthStore.ts
+++ composables/stores/useAuthStore.ts
@@ -8,28 +8,42 @@ export const useAuthStore = defineStore('auth', () => {
   const isLoadingAuth = ref(true);
 
   async function verifyAuth() {
+    const cookie = useCookie('pb_auth', {
+      path:     '/',
+      secure:   true,
+      sameSite: 'strict',
+      httpOnly: false, // change to "true" if you want only server-side access
+      maxAge:   604800,
+    });
+    
     if (!pb.authStore.isValid) {
-      isSignedIn.value = false;
+      logout();
       isLoadingAuth.value = false;
       return;
     }
-    isSignedIn.value = true
+    isSignedIn.value = true;
 
-    // Refresh auth just in case
     await pb.collection('users').authRefresh();
+
+    // This is final sign in
     if (pb.authStore.isValid) {
       isSignedIn.value = true;
+      cookie.value = JSON.stringify({
+        token: pb.authStore.token,
+        model: pb.authStore.model,
+      });
       isLoadingAuth.value = false;
       return;
     };
-    isSignedIn.value = false;
+    logout();
     isLoadingAuth.value = false;
   }
 
   async function logout() {
     if (pb.authStore.isValid) {
       pb.authStore.clear();
-      await verifyAuth();
+      useCookie('pb_auth').value = '';
+      isSignedIn.value = false;
       await navigateTo('/');
     }
   }
