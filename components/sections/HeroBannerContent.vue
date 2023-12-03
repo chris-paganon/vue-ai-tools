61063ecbba8dba8523bedc73a3d3453152117a6f
chrispaganon <info@chrispaganon.com>
2023-12-03 11:41:23 -0500

    Test new deployment

--- components/sections/HeroBannerContent.vue
+++ components/sections/HeroBannerContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full flex flex-column justify-content-center align-items-center">
-    <h1 class="text-center">VueAI.tools</h1>
+    <h1 class="text-center">VueAI.tools TEST</h1>
     <h2 class="text-2xl text-center">GPT-Powered Assistant for VueJS Documentation</h2>
   </div>
 </template>
