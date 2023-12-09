32e1d303e0367a5d31a98dcf1944207b5dbc1da4
chrispaganon <info@chrispaganon.com>
2023-12-09 13:13:35 -0500

    Move nav menu to seperate component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,27 +3,9 @@
     <NuxtLink to="/">
       <img src="/img/logo-nobg.png" alt="Logo" class="logo" width="60" />
     </NuxtLink>
-    <div v-if="!isLoadingAuth">
-      <div v-if="!isSignedIn" class="flex gap-4">
-        <Button @click="setIsLoginModalOpened(true)" severity="secondary" outlined>Login</Button>
-        <Button @click="setIsSignUpModalOpened(true)">Sign Up</Button>
-      </div>
-      <div v-else class="flex gap-4">
-        <Button @click="logout" link >Log out</Button>
-        <NuxtLink to="/dashboard">
-          <Button>Dashboard</Button>
-        </NuxtLink>
-      </div>
-    </div>
+    <NavMenu />
   </header>
 </template>
-
-<script setup lang="ts">
-const { isSignedIn, isLoadingAuth } = storeToRefs(useAuthStore());
-const { logout } = useAuthStore();
-
-const { setIsSignUpModalOpened, setIsLoginModalOpened } = useUIStore();
-</script>
 
 <style scoped lang="scss">
 header {
---
+++ components/NavMenu.vue
@@ -0,0 +1,21 @@
+<template>
+	<div v-if="!isLoadingAuth">
+		<div v-if="!isSignedIn" class="flex gap-4">
+			<Button @click="setIsLoginModalOpened(true)" severity="secondary" outlined>Login</Button>
+			<Button @click="setIsSignUpModalOpened(true)">Sign Up</Button>
+		</div>
+		<div v-else class="flex gap-4">
+			<Button @click="logout" link >Log out</Button>
+			<NuxtLink to="/dashboard">
+				<Button>Dashboard</Button>
+			</NuxtLink>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const { isSignedIn, isLoadingAuth } = storeToRefs(useAuthStore());
+const { logout } = useAuthStore();
+
+const { setIsSignUpModalOpened, setIsLoginModalOpened } = useUIStore();
+</script>
