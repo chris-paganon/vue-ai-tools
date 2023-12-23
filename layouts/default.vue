4fbe02bb95c5c1a305125b0282c32db354a337d6
chrispaganon <info@chrispaganon.com>
2023-12-23 13:03:16 -0500

    Add toast & auth popups to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="flex min-h-screen p-input-filled">
+		<Toast />
+    <AuthLoginPopup />
+    <AuthSignUpPopup />
 		<AppToolbar />
 		<div class="flex-grow-1 h-100 flex flex-column mx-3 mb-3">
 			<div class="flex justify-content-between align-items-center sm:justify-content-end py-2 sm:py-4">
