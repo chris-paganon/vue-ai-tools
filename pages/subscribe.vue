d273ba36c6996d3f233764b7dcadb2b883b4c4ee
chrispaganon <info@chrispaganon.com>
2023-12-25 13:53:19 -0500

    Better class name

--- pages/subscribe.vue
+++ pages/subscribe.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex justify-content-center">
-		<div class="max-w">
+		<div class="subscription-intro">
 			<h1>Help the dev!</h1>
 			<p class="mb-4">There are currently no paid features, concider this a donation. Your help is very much appreciated!</p>
 		</div>
@@ -9,7 +9,7 @@
 </template>
 
 <style scoped>
-.max-w {
+.subscription-intro {
 	max-width: 888px;
 	width: 100%;
 }
