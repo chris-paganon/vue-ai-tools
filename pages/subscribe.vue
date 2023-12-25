29ae31cb88ee9677dc3a0588349445c2c8e2111c
chrispaganon <info@chrispaganon.com>
2023-12-25 13:46:52 -0500

    Adjust subscribe page style

--- pages/subscribe.vue
+++ pages/subscribe.vue
@@ -1,5 +1,16 @@
 <template>
-	<h1>Help the dev!</h1>
-	<p class="mb-4">There are currently no paid features, concider this a donation. Your help is very much appreciated!</p>
+	<div class="flex justify-content-center">
+		<div class="max-w">
+			<h1>Help the dev!</h1>
+			<p class="mb-4">There are currently no paid features, concider this a donation. Your help is very much appreciated!</p>
+		</div>
+	</div>
 	<StripeCheckout />
 </template>
+
+<style scoped>
+.max-w {
+	max-width: 888px;
+	width: 100%;
+}
+</style>
