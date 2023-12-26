aa3927af59363bfe92b1b6a2f1d944e3086e2250
chrispaganon <info@chrispaganon.com>
2023-12-26 13:40:22 -0500

    Update transaction status after user redirect

--- server/api/checkoutSessionStatus.get.ts
+++ server/api/checkoutSessionStatus.get.ts
@@ -1,4 +1,5 @@
 import Stripe from 'stripe';
+import { useGetVerifiedUserPb } from '@/server/utils/useServerUtils';
 
 export default defineEventHandler(async (event) => {
 	const stripeSecretKey = useRuntimeConfig().stripeSecretKey;
@@ -12,10 +13,34 @@ export default defineEventHandler(async (event) => {
 	}
 
 	const stripe = new Stripe(stripeSecretKey);
-	const session = await stripe.checkout.sessions.retrieve(session_id);
 
-	return {
-		status: session.status,
-		customer_email: session.customer_details?.email,
-	};
+	try {
+		const session = await stripe.checkout.sessions.retrieve(session_id);
+	
+		const $pb = useGetVerifiedUserPb(event);
+		if (!$pb.authStore.model?.id) throw new Error('User id not found');
+	
+		const transaction = await $pb.collection('transactions').getFirstListItem(`session_id="${session.id}"`, {
+			fields: 'id',
+		});
+		// TODO: Match DB transaction status with Stripe session status
+		await $pb.collection('transactions').update(transaction.id, {
+			status: 'pending',
+		});
+	
+		return {
+			status: session.status,
+			customer_email: session.customer_details?.email,
+		};
+	} catch (error) {
+		if (! (error instanceof Error)) {
+			throw createError({
+				statusCode: 500,
+				statusMessage: 'Unknown error'
+			});
+		}
+		throw createError({
+			statusCode: 500,
+		});
+	}
 });
