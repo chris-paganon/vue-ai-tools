b6bfe0de18ca46a58e0009fe57447bc3cac40d91
chrispaganon <info@chrispaganon.com>
2023-12-26 13:02:47 -0500

    Add useGetVerifiedUserPb to server utils

--- server/utils/useServerUtils.ts
+++ server/utils/useServerUtils.ts
@@ -1,3 +1,6 @@
+import PocketBase from 'pocketbase';
+import { H3Event } from 'h3';
+
 export function fixTypesSerialization<T>(object: T[]): ({toJSON(): T[]}) {
   const data = {
     toJSON() {
@@ -5,4 +8,18 @@ export function fixTypesSerialization<T>(object: T[]): ({toJSON(): T[]}) {
     }
   }
   return data;
+}
+
+export function useGetVerifiedUserPb(event: H3Event) {
+  const pbUrl = useRuntimeConfig().public.pocketbaseUrl;
+  const pb = new PocketBase(pbUrl);
+  
+  const rawPbCookie = getCookie(event, 'pb_auth');
+  if (!rawPbCookie) return pb;
+  const pbCookie = JSON.parse(rawPbCookie);
+  if (!pbCookie.token || !pbCookie.model) return pb;
+  
+  pb.authStore.save(pbCookie.token, pbCookie.model);
+
+  return pb;
 }
