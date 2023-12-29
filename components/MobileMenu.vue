4c623f45c32afa68c7d851553ce3164dadbf2aaa
chrispaganon <info@chrispaganon.com>
2023-12-28 21:00:03 -0500

    Dismiss mobile sidebar on navigation

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -7,11 +7,11 @@
 			<div class="flex flex-column align-items-center gap-4">
 				<AuthLoginButtons />
 				<div v-if="isSignedIn" class="flex flex-column align-items-center gap-4 w-full">
-					<NuxtLink to="/dashboard" class="w-full">
+					<NuxtLink to="/dashboard" class="w-full" @click="mobileSidebarVisible = false">
 						<Button label="Dashboard" class="w-full" />
 					</NuxtLink>
 					<ToolbarChatHistory />
-					<NuxtLink to="/account" class="w-full">
+					<NuxtLink to="/account" class="w-full" @click="mobileSidebarVisible = false">
 						<Button outlined icon="pi pi-user" label="My account" aria-label="User" class="w-full" />
 					</NuxtLink>
 					<Button v-if="isSignedIn" @click="logout" link >Log out</Button>
