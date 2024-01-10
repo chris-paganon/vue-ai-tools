1082a30bc54b6ab9b63a0937c996cc2719b26891
chrispaganon <info@chrispaganon.com>
2024-01-10 00:23:59 -0500

    Add ChatConversation under templating tool

--- pages/tools/templating.vue
+++ pages/tools/templating.vue
@@ -2,6 +2,9 @@
   <div class="h-full flex flex-column">
 		<h1>Generate a Vue component from a JSON template:</h1>
 		<div ref="codeMirrorParent"></div>
+		<Button label="Generate" @click="generateComponent" class="mt-4" />
+		<ChatConversation />
+		<ChatInputControl v-if="messages.filter(message => message.role !== 'system').length > 0" />
 	</div>
 </template>
 
@@ -10,6 +13,10 @@ import { EditorState } from "@codemirror/state"
 import { EditorView, basicSetup } from "codemirror"
 import { json } from "@codemirror/lang-json";
 import { oneDark } from '@codemirror/theme-one-dark'
+
+const { messages } = storeToRefs(useChatStore());
+const { addUserMessage, addAssistantMessage } = useChatStore();
+const { setIsWaitingAnswer } = useChatInputStore();
 
 const codeMirrorParent = ref<HTMLDivElement | null>(null);
 const code = ref(`{
@@ -28,14 +35,33 @@ let startState = EditorState.create({
   extensions: extensions
 })
 
+const view = shallowRef<EditorView | null>(null);
 onMounted(() => {
 	console.log('test', codeMirrorParent.value);
 	if (!codeMirrorParent.value) return;
 	
-	let view = new EditorView({
+	view.value = new EditorView({
 		state: startState,
 		extensions: extensions,
 		parent: codeMirrorParent.value
 	})
 });
+
+async function generateComponent() {
+	if (!view.value) return;
+	console.log('generateComponent');
+	console.log('state: ', view.value?.state.doc.toString());
+
+	addUserMessage(`Use this JSON template to generate a Vue component: ${view.value.state.doc.toString()}`);
+  setIsWaitingAnswer(true);
+
+  const assistantAnswer = await useAskQuestion();
+  if (!assistantAnswer) {
+    setIsWaitingAnswer(false);
+    return;
+  };
+
+  addAssistantMessage(assistantAnswer);
+  setIsWaitingAnswer(false);
+}
 </script>
