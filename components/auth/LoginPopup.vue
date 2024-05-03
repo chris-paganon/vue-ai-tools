e84c2e9f7ca3e8beceed8931a515b2c765a0c8bd
chrispaganon <info@chrispaganon.com>
2024-05-03 12:58:21 -0400

    Remove useless header content type JSON

--- components/auth/LoginPopup.vue
+++ components/auth/LoginPopup.vue
@@ -68,9 +68,6 @@ async function login() {
     errorMessage.value = '';
     await $fetch('/api/auth/signin', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
       body: JSON.stringify({
         email: email.value,
         password: password.value,
