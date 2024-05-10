7494a6ed624386ce1c7106ba900a14d7138d6e6f
chrispaganon <info@chrispaganon.com>
2024-05-10 12:56:18 -0400

    Dont run migrations in dev

--- server/plugins/00-migrate.ts
+++ server/plugins/00-migrate.ts
@@ -2,7 +2,10 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import Database from 'better-sqlite3';
 
-export default defineNitroPlugin((nitroApp) => {
+export default defineNitroPlugin(() => {
+  if (import.meta.dev) {
+    return;
+  }
   const betterSqlite = new Database('db/sqlite.db');
   const db = drizzle(betterSqlite);
   console.log('Migrating database');
