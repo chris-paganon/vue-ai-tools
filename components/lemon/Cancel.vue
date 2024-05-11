9d997989b1fa04fe5607c3eb71612889d5a62d56
chrispaganon <info@chrispaganon.com>
2024-05-11 12:42:31 -0400

    Add confirmation dialog to cancel subscription

--- components/lemon/Cancel.vue
+++ components/lemon/Cancel.vue
@@ -1,10 +1,20 @@
 <template>
-  <Button outlined :loading="isLoading" @click="cancelSubscription()"
+  <Button outlined :loading="isLoading" @click="isConfirmationOpened = true"
     >Cancel my subscription</Button
   >
+  <ConfirmDialog
+    v-model="isConfirmationOpened"
+    title="Cancel Subscription"
+    message="Are you sure you want to cancel your subscription?"
+    confirm-label="Yes, cancel"
+    cancel-label="No, keep it"
+    @confirmed="cancelSubscription"
+  />
 </template>
 
 <script setup lang="ts">
+const isConfirmationOpened = ref(false);
+
 const isLoading = ref(false);
 async function cancelSubscription() {
   isLoading.value = true;
