d63418edf50e0eff4c4a59a989e8022aed586bd3
chrispaganon <info@chrispaganon.com>
2024-05-11 15:43:56 -0400

    Manual migration to change existing column type of subscription id

--- db/migrations/0001_late_leo.sql
+++ db/migrations/0001_late_leo.sql
@@ -1,10 +1,2 @@
 DROP INDEX IF EXISTS `subscriptions_stripe_id_unique`;--> statement-breakpoint
-/*
- SQLite does not support "Changing existing column type" out of the box, we do not generate automatic migration for that, so it has to be done manually
- Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
-                  https://www.sqlite.org/lang_altertable.html
-                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
-
- Due to that we don't generate migration automatically and it has to be done manually
-*/--> statement-breakpoint
 ALTER TABLE `subscriptions` DROP COLUMN `stripe_id`;
