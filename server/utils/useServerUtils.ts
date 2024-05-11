42e0649bd6b91099eb2a3b35e4d17cc4681487c9
chrispaganon <info@chrispaganon.com>
2024-05-11 12:01:23 -0400

    Move getDrizzleDb to server utils

--- server/utils/useServerUtils.ts
+++ server/utils/useServerUtils.ts
@@ -1,3 +1,11 @@
+import sqlite from 'better-sqlite3';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+
+export function getDrizzleDb() {
+  const sqliteDB = sqlite('db/sqlite.db');
+  return drizzle(sqliteDB);
+}
+
 export function fixTypesSerialization<T>(object: T[]): { toJSON(): T[] } {
   const data = {
     toJSON() {
