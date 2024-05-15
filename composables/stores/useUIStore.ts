0008fab888d5d759c9070607dd647f44f1d97024
chrispaganon <info@chrispaganon.com>
2024-05-15 15:01:53 -0400

    Reorganize useUIStore code

--- composables/stores/useUIStore.ts
+++ composables/stores/useUIStore.ts
@@ -1,14 +1,15 @@
 export const useUIStore = defineStore('ui', () => {
   const isChatOpened = ref(false);
-  const isLoginModalOpened = ref(false);
-  const isSignUpModalOpened = ref(false);
-
   function setIsChatOpened(value: boolean) {
     isChatOpened.value = value;
   }
+
+  const isLoginModalOpened = ref(false);
   function setIsLoginModalOpened(value: boolean) {
     isLoginModalOpened.value = value;
   }
+
+  const isSignUpModalOpened = ref(false);
   function setIsSignUpModalOpened(value: boolean) {
     isSignUpModalOpened.value = value;
   }
