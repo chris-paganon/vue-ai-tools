2ae68a33297d4546282e3523750d4c61bfc766cb
chrispaganon <info@chrispaganon.com>
2024-06-18 16:08:21 -0400

    Adjust introduction content & remove gradient background

--- components/sections/Introduction.vue
+++ components/sections/Introduction.vue
@@ -1,20 +1,19 @@
 <template>
-  <section class="max-w-1024 mx-auto px-2 md:px-4 pt-6">
-    <p class="mb-6">
-      Your go-to resource for AI tools tailored specifically for VueJS. Our
-      GPT-Powered assistant is fine tuned to provide
-      <strong>more helpful answers</strong> and to
+  <section class="max-w-1024 mx-auto px-2 md:px-4 py-6">
+    <p>
+      Your go-to resource for AI tools tailored specifically for VueJS. Our open
+      source AI assistant uses the Vue documentation to provide more helpful
+      answers, <strong>links to the docs</strong> and to
       <strong>minimize errors and hallucinations</strong>. The perfect assistant
       to <strong>help you start</strong> you VueJS journey.
     </p>
   </section>
-  <section class="bg-secondary-gradient my-2 py-3 md:py-6">
+  <section class="surface-card my-2 py-3 md:py-6">
     <div class="max-w-1024 mx-auto px-2 md:px-4">
       <div class="grid">
         <div
           class="col-12 md:col-6 flex flex-column justify-content-center gap-3 lg:gap-4 pr-4"
         >
-          <h3 class="text-center md:text-left">New</h3>
           <p>
             Build components faster & more reliably with our free template
             builder.
@@ -31,7 +30,10 @@
             class="flex flex-column gap-3 lg:gap-4"
           >
             <Divider />
-            <p>Subscribe to get GPT-4o responses instead of GPT-3.</p>
+            <p>
+              Subscribe to get Command R+ responses instead of Command R
+              (similar to going from GPT 3.5 turbo to GPT-4).
+            </p>
             <NuxtLink to="/account">
               <Button label="Subscribe now" class="w-full" />
             </NuxtLink>
@@ -54,14 +56,3 @@
 const { isSignedIn, isSubscribed } = storeToRefs(useAuthStore());
 const { setIsSignUpModalOpened } = useUIStore();
 </script>
-
-<style scoped>
-.bg-secondary-gradient {
-  background: linear-gradient(
-    var(--surface-ground) 0%,
-    var(--surface-f) 10%,
-    var(--surface-f) 90%,
-    var(--surface-ground) 100%
-  );
-}
-</style>
