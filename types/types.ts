3a6fd2fd9fcb951c5293bc40900e493e8ae33b2f
chrispaganon <info@chrispaganon.com>
2024-07-04 12:41:10 -0400

    Remove some old stripe types

--- types/types.ts
+++ types/types.ts
@@ -1,4 +1,3 @@
-import type Stripe from 'stripe';
 import type { MenuItemCommandEvent } from 'primevue/menuitem';
 import type { ChatMessage } from 'llamaindex';
 
@@ -70,52 +69,6 @@ export function isChatCompletionMessages(
   messages: unknown[]
 ): messages is ChatCompletionMessage[] {
   return messages.every(isChatCompletionMessage);
-}
-
-/**
- * Stripe
- */
-export type EnabledStripeWebhookEvents =
-  | Stripe.CheckoutSessionCompletedEvent
-  | Stripe.CheckoutSessionExpiredEvent
-  | Stripe.CustomerSubscriptionCreatedEvent
-  | Stripe.CustomerSubscriptionUpdatedEvent
-  | Stripe.CustomerSubscriptionDeletedEvent;
-
-export function isEnabledStripeWebhookEvents(
-  event: Stripe.Event
-): event is EnabledStripeWebhookEvents {
-  return (
-    event.type === 'checkout.session.completed' ||
-    event.type === 'checkout.session.expired' ||
-    event.type === 'customer.subscription.created' ||
-    event.type === 'customer.subscription.updated' ||
-    event.type === 'customer.subscription.deleted'
-  );
-}
-
-export function isSubscriptionCreatedEvent(
-  event: Stripe.Event
-): event is Stripe.CustomerSubscriptionCreatedEvent {
-  return event.type === 'customer.subscription.created';
-}
-export function isSubscriptionUpdatedEvent(
-  event: Stripe.Event
-): event is Stripe.CustomerSubscriptionUpdatedEvent {
-  return event.type === 'customer.subscription.updated';
-}
-
-export function isStripeCustomer(
-  event: Stripe.Customer | Stripe.DeletedCustomer | string
-): event is Stripe.Customer {
-  if (typeof event === 'string') return false;
-  return event.object === 'customer' && !event.deleted;
-}
-export function isStripeDeletedCustomer(
-  event: Stripe.Customer | Stripe.DeletedCustomer | string
-): event is Stripe.DeletedCustomer {
-  if (typeof event === 'string') return false;
-  return event.deleted === true;
 }
 
 export interface InputOptionTabs {
