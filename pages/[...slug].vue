7de0d334bf8d246ad0007c2b64eff5203db1ea0c
chrispaganon <info@chrispaganon.com>
2024-12-07 13:23:29 +0100

    add 404 fallback

---
+++ pages/[...slug].vue
@@ -0,0 +1,18 @@
+<template>
+  <main
+    class="flex-grow-1 flex flex-column align-items-center justify-content-center"
+  >
+    <h1>404, this page doesn't exist</h1>
+    <Button
+      @click="$router.push('/')"
+      label="Go back to the home page"
+      class="w-fit"
+    />
+  </main>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home',
+});
+</script>
