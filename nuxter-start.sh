118422364b859708f5c7a24d9ae271452cbf0e42
chrispaganon <info@chrispaganon.com>
2025-10-12 18:03:01 +0200

    move configs to top level

--- nuxter-start.sh
+++ nuxter-start.sh
@@ -7,10 +7,10 @@ projectNameStaging="$projectName-staging"
 # Check if --local flag is provided
 if [[ "$*" == *"--local"* ]]; then
     echo "Starting in local mode..."
-    docker compose -f ./nuxter-config/nuxter-base-compose.yml -f ./nuxter-config/nuxter-base-compose.override.yml --env-file ./nuxter-config/staging.env -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
-    docker compose -f ./nuxter-config/nuxter-base-compose.yml -f ./nuxter-config/nuxter-base-compose.override.yml --env-file ./nuxter-config/production.env -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
+    docker compose -f ./docker-compose.nuxter.yml -f ./docker-compose.nuxter.override.yml --env-file ./.env.staging -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
+    docker compose -f ./docker-compose.nuxter.yml -f ./docker-compose.nuxter.override.yml --env-file ./.env.production -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
 else
     echo "Starting production and staging environments..."
-    docker compose -f ./nuxter-config/nuxter-base-compose.yml --env-file ./nuxter-config/production.env -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
-    docker compose -f ./nuxter-config/nuxter-base-compose.yml --env-file ./nuxter-config/staging.env -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
+    docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.production -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
+    docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.staging -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
 fi
