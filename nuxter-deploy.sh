af3ce10f82cb641c42fa147730306b7c6669acb9
chrispaganon <info@chrispaganon.com>
2025-10-13 13:29:30 +0200

    enable separate deployments

--- nuxter-deploy.sh
+++ nuxter-deploy.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-rsync -avzre 'ssh' docker-compose.nuxter.yml Dockerfile nuxter-start.sh \
+if [[ "$*" == *"--production"* ]]; then
+    echo "Deploying production environment..."
+    rsync -azre 'ssh' docker-compose.nuxter.yml Dockerfile nuxter-start.sh \
+      .env.production \
+      .output.production \
+      nuxter@77.37.87.199:~/nuxter/vue-ai-tools
+    
+    ssh nuxter@77.37.87.199 "cd ~/nuxter/vue-ai-tools && ./nuxter-start.sh --production"
+    exit 0
+fi
+
+if [[ "$*" == *"--staging"* ]]; then
+    echo "Deploying staging environment..."
+
+    rsync -azre 'ssh' docker-compose.nuxter.yml Dockerfile nuxter-start.sh \
+      .env.staging \
+      .output.staging \
+      nuxter@77.37.87.199:~/nuxter/vue-ai-tools
+
+    ssh nuxter@77.37.87.199 "cd ~/nuxter/vue-ai-tools && ./nuxter-start.sh --staging"
+    exit 0
+fi
+
+echo "Deploying production and staging environments..."
+
+rsync -azre 'ssh' docker-compose.nuxter.yml Dockerfile nuxter-start.sh \
   .env.production .env.staging \
   .output.production .output.staging \
   nuxter@77.37.87.199:~/nuxter/vue-ai-tools
--- nuxter-start.sh
+++ nuxter-start.sh
@@ -9,8 +9,21 @@ if [[ "$*" == *"--local"* ]]; then
     echo "Starting in local mode..."
     docker compose -f ./docker-compose.nuxter.yml -f ./docker-compose.nuxter.override.yml --env-file ./.env.staging -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
     docker compose -f ./docker-compose.nuxter.yml -f ./docker-compose.nuxter.override.yml --env-file ./.env.production -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
-else
-    echo "Starting production and staging environments..."
-    docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.production -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
-    docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.staging -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
+    exit 0
 fi
+
+if [[ "$*" == *"--production"* ]]; then
+    echo "Starting production environment..."
+    docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.production -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
+    exit 0
+fi
+
+if [[ "$*" == *"--staging"* ]]; then
+    echo "Starting staging environment..."
+    docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.staging -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
+    exit 0
+fi
+
+echo "Starting production and staging environments..."
+docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.production -p ${projectNameProduction} --project-directory ./ up --build --remove-orphans -d
+docker compose -f ./docker-compose.nuxter.yml --env-file ./.env.staging -p ${projectNameStaging} --project-directory ./ up --build --remove-orphans -d
