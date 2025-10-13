efa5835ef23c4f6d0930ae7007bc009466ba6b6a
chrispaganon <info@chrispaganon.com>
2025-10-13 21:46:35 +0200

    update umami id & url

--- plugins/00.headMeta.ts
+++ plugins/00.headMeta.ts
@@ -6,8 +6,9 @@ export default defineNuxtPlugin(() => {
     script: [
       {
         async: true,
-        src: 'https://umami.vueai.tools/script.js',
-        'data-website-id': '9a36b898-44ef-4267-89d6-dd7a3afb90a2',
+        src: 'https://umami.chrispaganon.com/script.js',
+        'data-website-id': 'c52b9daa-f6ed-4cfb-a2e0-d73f5a60e1f5',
+        'data-do-not-track': true,
       },
     ],
   });
